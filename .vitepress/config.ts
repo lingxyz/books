241e17a07736bd3177f738cc5e4ae29ed5e828e4
Ling Z <771152260@qq.com>
2023-05-29 06:57:44 +0000

    fix: favicon.ico

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   head: [
     [
       'link',
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/books/assets/favicon.ico' }
     ],
   ],
   themeConfig: {
